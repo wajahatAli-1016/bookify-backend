0160bade06698de1cce0491acfc2ceb37a5a44df
wajahatAli-1016 <wajahatalizaidi35@gmail.com>
2025-05-11 21:32:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const mongoose = require('mongoose');
-const port =process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
 require('dotenv').config()
 
+// middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173','https://bookify-frontend-vw31.vercel.app'],
-  credentials: true
+    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    credentials: true
 }))
 
-const bookRoutes = require('./src/books/book.route')
+// routes
+const bookRoutes = require('./src/books/book.route');
+const orderRoutes = require("./src/orders/order.route")
+const userRoutes =  require("./src/users/user.route")
+const adminRoutes = require("./src/stats/admin.stats")
 
-const orderRoutes = require('./src/orders/order.route')
-const adminRoutes = require('./src/stats/admin.stats')
-
-const userRoutes = require('./src/users/user.route')
 app.use("/api/books", bookRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
 app.use("/api/admin", adminRoutes)
 
 async function main() {
-    await mongoose.connect(process.env.DB_URL);
-    app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-  }
-  main().then(()=> console.log("Mongodb connected successfully")).catch(err => console.log(err));
+  await mongoose.connect(process.env.DB_URL);
+  app.use("/", (req, res) => {
+    res.send("Book Store Server is running!");
+  });
+}
+
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
