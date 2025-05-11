c6adad6112de1329d95593c36677fad6f15f8551
wajahatAli-1016 <wajahatalizaidi35@gmail.com>
2025-05-11 22:07:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    origin: ['http://localhost:5173', 'https://bookify-frontend-vw31.vercel.app'],
     credentials: true
 }))
 
