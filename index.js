30670291cd766fd931a53ef923e7dd0c0a8085cf
wajahatAli-1016 <wajahatalizaidi35@gmail.com>
2025-05-11 21:50:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
 app.use("/api/admin", adminRoutes)
 
+
+app.get("/", (req, res) => {
+  res.send("Book Store Server is running!");
+});
+
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
+  console.log("MongoDB connected successfully!");
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main().catch(err => console.log("MongoDB connection error:", err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
